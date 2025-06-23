2055d2fff893ea569c8032e73cf716367c828f4a
Regine Valmont <sarahsmithregine90@gmail.com>
2025-06-23 00:47:48 -0400

    Rendered

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const communityData = require("./data/communityData");
 
 app.use(morgan("combined"));
 app.use(helmet());
-app.use(cors());
+app.use(cors({ credentials: true, origin: true }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 
 async function main() {
   try {
-    await mongoose.connect(process.env.DB_URL);
+    await mongoose.connect(
+      "mongodb+srv://Regine90:Sarahlove9@@cleanwithcare.gx79foe.mongodb.net/?retryWrites=true&w=majority&appName=CleanWithCare");
     console.log(`MongoDB database is connected.`);
   } catch (error) {
     console.log(`There was an error connecting to the database: ${error}`);
