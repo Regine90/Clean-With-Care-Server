dcc8e0ddb155fcd8f97059790694ea18d26b66be
Regine Valmont <sarahsmithregine90@gmail.com>
2025-06-23 22:52:55 -0400

    bdvscvss

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,24 +1,11 @@
-const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
-
-// Define User Schema (only once in your app)
-const userSchema = new mongoose.Schema({
-  firstName: String,
-  lastName: String,
-  username: { type: String, unique: true }, // email as username
-  password: String,
-  createdAt: { type: Date, default: Date.now },
-});
-
-const User = mongoose.model("User", userSchema);
+const User = require("../models/userModel"); 
 
 // REGISTER Controller
 const register = async (req, res, next) => {
   const { firstName, lastName, username, password } = req.body;
-  console.log({ firstName, lastName, username, password });
 
   try {
-    // Check if user already exists
     const existingUser = await User.findOne({ username });
     if (existingUser) {
       return res.status(400).json({
@@ -26,10 +13,7 @@ const register = async (req, res, next) => {
       });
     }
 
-    // Hash the password
     const hashedPassword = await bcrypt.hash(password, 10);
-
-    // Create and save new user
     const newUser = new User({
       firstName,
       lastName,
@@ -38,8 +22,6 @@ const register = async (req, res, next) => {
     });
 
     await newUser.save();
-
-    console.log("User successfully created");
 
     return res.status(201).json({
       success: { message: "New user created successfully!" },
@@ -58,7 +40,6 @@ const login = async (req, res, next) => {
   const { username, password } = req.body;
 
   try {
-    // Find user by username (email)
     const user = await User.findOne({ username });
     if (!user) {
       return res.status(400).json({
@@ -66,15 +47,12 @@ const login = async (req, res, next) => {
       });
     }
 
-    // Compare passwords
     const valid = await bcrypt.compare(password, user.password);
     if (!valid) {
       return res.status(400).json({
         error: { message: "Invalid username or password." },
       });
     }
-
-    console.log("Login successful");
 
     return res.status(200).json({
       success: { message: "Login successful." },
@@ -88,27 +66,12 @@ const login = async (req, res, next) => {
   }
 };
 
-// LOGOUT Controller (same as before)
+// LOGOUT Controller
 const logout = async (req, res, next) => {
-  console.log("Initializing logout controller logic...");
   res.clearCookie("connect.sid");
-  function sessionDestruction(err) {
-    if (err) {
-      return next(err);
-    }
-  }
-  sessionDestruction();
-  console.log("Logout function activated. Logging out...");
   return res.status(200).json({
     success: { message: "User logged out" },
   });
 };
 
-// LOCAL LOGIN Stub
-const localLogin = async (req, res, next) => {
-  return res.status(200).json({
-    success: { message: "Local login successful." },
-  });
-};
-
-module.exports = { register, login, logout, localLogin };
+module.exports = { register, login, logout };
--- models/userModel.js
+++ models/userModel.js
@@ -32,6 +32,4 @@ const userSchema = new Schema({
   },
 });
 
-const User = mongoose.model("User", userSchema);
-
-module.exports = User;
+module.exports = mongoose.models.User || mongoose.model("User", userSchema);
